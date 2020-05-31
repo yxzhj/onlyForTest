92b30329dde24cf59a9fd726eaaee5a2c6284c5d
unknown <1076613402@qq.com>
2020-05-31 15:59:17 +0800

    learn python the hard way to

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print('hello world\n')      #多打印一行
+print('hello',end='')       #只打印一行
+print('world')
+
+'''
+print(*objects,sep='',end='\n',file=sys.stdout,flush=False)
+objects--复数，表示可输出多个对象
+file--要写入的文件对象
+'''
